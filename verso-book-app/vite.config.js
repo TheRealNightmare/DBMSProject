3ad813b7a754aecb5130c255612eebcbd0a9bf0d
TheRealNightmare <nnahid929@gmail.com>
2026-01-10 21:03:51 +0600

    changed tailwind config

--- verso-book-app/vite.config.js
+++ verso-book-app/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+
+// Define __dirname for ES module scope
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
