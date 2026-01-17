62cc9a3680d2a2912310af01e107228d82da3f29
TheRealNightmare <nnahid929@gmail.com>
2026-01-17 20:54:26 +0600

    hsitory page route fixes and model relations added

--- verso-backend/app/Models/Book.php
+++ verso-backend/app/Models/Book.php
@@ -6,11 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $primaryKey = 'book_id';
-    // Removed 'author_name' from fillable
+    
     protected $fillable = ['title', 'description', 'cover_image', 'category', 'rating_avg','content_path','publication_date'];
 
     public function authors()
     {
         return $this->belongsToMany(Author::class, 'book_author', 'book_id', 'author_id');
+    }
+
+    /**
+     * Get the shelf items associated with the book.
+     */
+    public function shelfItems()
+    {
+        return $this->hasMany(ShelfItem::class, 'book_id');
     }
 }
